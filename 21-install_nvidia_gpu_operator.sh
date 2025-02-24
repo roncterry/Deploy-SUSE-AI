59bd8b558ae82449b578cd96576d4af5775614d0
roncterry <15840212+roncterry@users.noreply.github.com>
2025-02-24 12:18:11 -0700

    switched to installing the Helm chart directly
    Rather than install using a HelmChart object type and the Helm operator to install the operator now the NVIDIA GPU Operator Helm repo is added and the Helm chart is installed directly from the repo. The function that installed using a HelmChart object type is still included in the code.

--- 21-install_nvidia_gpu_operator.sh
+++ 21-install_nvidia_gpu_operator.sh
@@ -26,7 +26,24 @@ fi
 
 ##############################################################################
 
-deploy_nvidia_gpu_operator() {
+# You can either source in the variables from a common config file or
+# set the them in this script.
+
+CONFIG_FILE=deploy_suse_ai.cfg
+
+if ! [ -z ${CONFIG_FILE} ]
+then
+  if [ -e ${CONFIG_FILE} ]
+  then
+    source ${CONFIG_FILE}
+  fi
+else
+  NVIDIA_GPU_OPERATOR_REPO_URL=https://helm.ngc.nvidia.com/nvidia
+fi
+
+##############################################################################
+
+deploy_nvidia_gpu_operator_via_the_helm_operator() {
   echo "Writing out nvidia-gpu-operator.yaml ..."
   echo
 
@@ -37,7 +54,7 @@ metadata:
   name: gpu-operator
   namespace: kube-system
 spec:
-  repo: https://helm.ngc.nvidia.com/nvidia
+  repo: ${NVIDIA_GPU_OPERATOR_REPO_URL}
   chart: gpu-operator
   targetNamespace: gpu-operator
   createNamespace: true
@@ -51,7 +68,7 @@ spec:
       - name: CONTAINERD_RUNTIME_CLASS
         value: nvidia
       - name: CONTAINERD_SET_AS_DEFAULT
-        value: true
+        value: \"true\"
 " > nvidia-gpu-operator.yaml
 
   cat nvidia-gpu-operator.yaml
@@ -60,7 +77,42 @@ spec:
   echo "COMMAND: kubectl apply -f nvidia-gpu-operator.yaml"
   kubectl apply -f nvidia-gpu-operator.yaml
   echo
+}
 
+deploy_nvidia_gpu_operator() {
+  echo "Writing out nvidia-gpu-operator-values.yaml ..."
+  echo
+
+  echo "
+toolkit:
+  env:
+  - name: CONTAINERD_SOCKET
+    value: /run/k3s/containerd/containerd.sock
+  - name: CONTAINERD_CONFIG
+    value: /var/lib/rancher/rke2/agent/etc/containerd/config.toml.tmpl
+  - name: CONTAINERD_RUNTIME_CLASS
+    value: nvidia
+  - name: CONTAINERD_SET_AS_DEFAULT
+    value: \"true\"
+" > nvidia-gpu-operator-values.yaml
+
+  cat nvidia-gpu-operator-values.yaml
+  echo
+
+  echo "COMMAND: helm repo add nvidia ${NVIDIA_GPU_OPERATOR_REPO_URL}"
+  helm repo add nvidia ${NVIDIA_GPU_OPERATOR_REPO_URL}
+  echo
+
+  echo "COMMAND: helm repo update"
+  helm repo update
+  echo
+
+  echo "COMMAND: helm install gpu-operator -n gpu-operator --create-namespace --set driver.enabled=false -f nvidia-gpu-operator-values.yaml nvidia/gpu-operator"
+  helm install gpu-operator -n gpu-operator --create-namespace --set driver.enabled=false -f nvidia-gpu-operator-values.yaml nvidia/gpu-operator
+  echo
+}
+
+show_nvidia_gpu_operator_deployment_status() {
   echo -n "Waiting for namespace to be created "
   until kubectl get namespaces | grep -q gpu-operator
   do
@@ -70,10 +122,39 @@ spec:
   echo "."
   echo
 
+  echo -n "Waiting for gpu-operator deployment to be started "
+  until kubectl -n gpu-operator get deployment | grep -q gpu-operator
+  do
+    echo -n "."
+    sleep 2
+  done
+  echo "."
+  echo
+
+  echo "COMMAND: kubectl -n gpu-operator rollout status deploy/gpu-operator-node-feature-discovery-gc"
+  kubectl -n gpu-operator rollout status deploy/gpu-operator-node-feature-discovery-gc
+  echo
+
+  echo "COMMAND: kubectl -n gpu-operator rollout status deploy/gpu-operator-node-feature-discovery-master"
+  kubectl -n gpu-operator rollout status deploy/gpu-operator-node-feature-discovery-master
+  echo
+
   echo "COMMAND: kubectl -n gpu-operator rollout status deploy/gpu-operator"
   kubectl -n gpu-operator rollout status deploy/gpu-operator
   echo
-  
+
+  echo -n "Waiting for the nvidia-operator-validator pod to become ready "
+  until kubectl -n gpu-operator get pods | grep nvidia-operator-validator | grep -q "Running"
+  do
+    echo -n "."
+    sleep 2
+  done
+  echo "."
+  echo
+
+  echo "Waiting for the metadata labels to be created/updated ..."
+  sleep 15
+  echo
 }
 
 verify_nvidia_gpu_operator_deployment() {
@@ -131,6 +212,8 @@ verify_nvidia_gpu_operator_deployment() {
     else
       echo GPU_NODE=false
       echo
+      echo "Note: If you think this is incorrect wait about 10-15 seconds and run"
+      echo "      this script again. The metadata labels may not have been updated yet."
       echo
     fi
 done
@@ -140,8 +223,9 @@ done
 
 if ! kubectl get pods -A | grep -q nvidia-operator-validator
 then
+  #deploy_nvidia_gpu_operator_via_the_helm_operator
   deploy_nvidia_gpu_operator
-  sleep 5
+  show_nvidia_gpu_operator_deployment_status
 fi
 
 verify_nvidia_gpu_operator_deployment
