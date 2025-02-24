d651934c8fb106eea279c0bcdfe7df96df087c5b
roncterry <15840212+roncterry@users.noreply.github.com>
2025-02-24 15:00:04 -0700

    better status output

--- 98-reset_cluster.sh
+++ 98-reset_cluster.sh
@@ -2,10 +2,29 @@
 
 source deploy_suse_ai.cfg
 
+echo
+echo "Resetting the Cluster ..."
+echo
+echo "COMMAND: helm -n ${SUSE_AI_NAMESPACE} uninstall open-webui"
 helm -n ${SUSE_AI_NAMESPACE} uninstall open-webui
-helm -n ${SUSE_AI_NAMESPACE} uninstall ollama
-helm -n ${SUSE_AI_NAMESPACE} uninstall milvus
-kubectl delete namespace ${SUSE_AI_NAMESPACE}
+echo
 
+echo "COMMAND: helm -n ${SUSE_AI_NAMESPACE} uninstall ollama"
+helm -n ${SUSE_AI_NAMESPACE} uninstall ollama
+echo
+
+echo "COMMAND: helm -n ${SUSE_AI_NAMESPACE} uninstall milvus"
+helm -n ${SUSE_AI_NAMESPACE} uninstall milvus
+echo
+
+echo "COMMAND: kubectl delete namespace ${SUSE_AI_NAMESPACE}"
+kubectl delete namespace ${SUSE_AI_NAMESPACE}
+echo
+
+echo "COMMAND: helm -n gpu-operator uninstall gpu-operator"
 helm -n gpu-operator uninstall gpu-operator
+echo
+
+echo "COMMAND: kubectl delete namespace gpu-operator"
 kubectl delete namespace gpu-operator
+echo
