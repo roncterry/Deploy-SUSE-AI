0be886bc887e934a10501bd94e32b366b14d7ddf
roncterry <15840212+roncterry@users.noreply.github.com>
2025-03-20 11:15:10 -0600

    Can now deploy using Let's Encrypt or secret for TLS

--- 02-deploy_rancher_with_helm.sh
+++ 02-deploy_rancher_with_helm.sh
@@ -22,6 +22,13 @@ else
   RANCHER_HOSTNAME="rancher.example.com"
   RANCHER_ADMIN_PW="rancher"
   RANCHER_REPLICAS=1
+  RANCHER_TLS_SOURCE="rancher"
+  RANCHER_TLS_EMAIL="admin@example.com"
+  RANCHER_TLS_INGRESS_CLASS="nginx"
+  RANCHER_TLS_CERT_FILE=
+  RANCHER_TLS_KEY_FILE=
+  RANCHER_TLS_CA_FILE=
+  RANCHER_TLS_PRIVATE_CA=true
 fi
 
 ##############################################################################
@@ -95,8 +102,8 @@ install_certmanager() {
   helm repo update
 
   echo
-  echo "COMMAND: helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true ${CERTMANAGER_VERSION}"
-  helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true ${CERTMANAGER_VERSION}
+  echo "COMMAND: helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VERSION}"
+  helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VERSION}
 
   echo
   echo "COMMAND: kubectl -n cert-manager rollout status deploy/cert-manager"
@@ -107,16 +114,46 @@ install_rancher() {
   echo
   echo "Installing Rancher ..."
   echo "------------------------------------------------------------"
+
+  case ${RANCHER_TLS_SOURCE} in
+    letsEncrypt)
+      echo "Retreiving the Let's Encrypt CA certificate ..."
+      echo "COMMAND: curl https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem --output cacerts.pem"
+      curl https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem --output cacerts.pem
+      echo
+
+      echo "Creating secret for the CA cert ..."
+      echo "COMMAND: kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem"
+      kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem
+
+      RANCHER_CERT_ARGS="--set ingress.tls.source=letsEncrypt --set letsEncrypt.email=${RANCHER_TLS_EMAIL} --set letsEncrypt.ingress.class=${RANCHER_TLS_INGRESS_CLASS} --set privateCA=true"
+      echo
+    ;;
+    secret)
+      echo "Creating TLS secret ..."
+      echo "COMMAND: kubectl -n cattle-system create secret tls tls-rancher-ingress --cert=${RANCHER_TLS_CERT_FILE} --key=${RANCHER_TLS_KEY_FILE}"
+      kubectl -n cattle-system create secret tls tls-rancher-ingress --cert=${RANCHER_TLS_CERT_FILE} --key=${RANCHER_TLS_KEY_FILE}
+      echo
+
+      echo "Creating secret for the CA cert ..."
+      echo "COMMAND: kubectl -n cattle-system create secret generic tls-ca --from-file=${RANCHER_TLS_CA_FILE}"
+      kubectl -n cattle-system create secret generic tls-ca --from-file=${RANCHER_TLS_CA_FILE}
+
+      RANCHER_CERT_ARGS="--set ingress.tls.source=secret --set privateCA=${RANCHER_TLS_PRIVATE_CA}"
+      echo
+    ;;
+  esac
+
   echo "COMMAND: 
-  helm repo add rancher-latest ${RANCHER_HELM_REPO}
+  helm repo add rancher-prime ${RANCHER_HELM_REPO}
   helm repo update"
 
-  helm repo add rancher-manager ${RANCHER_HELM_REPO}
+  helm repo add rancher-prime ${RANCHER_HELM_REPO}
   helm repo update
 
   echo
-  echo "COMMAND: helm install rancher rancher-manager/rancher --namespace cattle-system --create-namespace --set hostname=${RANCHER_HOSTNAME} --set bootstrapPassword=${RANCHER_ADMIN_PW} --set replicas=${RANCHER_REPLICAS}"
-  helm install rancher rancher-manager/rancher --namespace cattle-system --create-namespace --set hostname=${RANCHER_HOSTNAME} --set bootstrapPassword=${RANCHER_ADMIN_PW} --set replicas=${RANCHER_REPLICAS}
+  echo "COMMAND: helm upgrade --install rancher rancher-prime/rancher --namespace cattle-system --create-namespace --set hostname=${RANCHER_HOSTNAME} --set bootstrapPassword=${RANCHER_ADMIN_PW} --set replicas=${RANCHER_REPLICAS} ${RANCHER_CERT_ARGS}"
+  helm upgrade --install rancher rancher-prime/rancher --namespace cattle-system --create-namespace --set hostname=${RANCHER_HOSTNAME} --set bootstrapPassword=${RANCHER_ADMIN_PW} --set replicas=${RANCHER_REPLICAS} ${RANCHER_CERT_ARGS}
 
   echo
   echo "COMMAND: kubectl -n cattle-system rollout status deploy/rancher"
@@ -132,6 +169,13 @@ install_rancher() {
 #   Main Code Body
 ###############################################################################
 
-install_certmanager
-install_rancher
+case ${RANCHER_TLS_SOURCE} in
+  secret)
+    install_rancher
+  ;;
+  *)
+    install_certmanager
+    install_rancher
+  ;;
+esac
 
