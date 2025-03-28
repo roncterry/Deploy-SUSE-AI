8da3e27c4df116b41fc1301cb2ace7f252df2566
roncterry <15840212+roncterry@users.noreply.github.com>
2025-03-27 21:11:52 -0600

    add NVIDIA driver repo and install driver if needed

--- 10-install_nvidia_compute_utils.sh
+++ 10-install_nvidia_compute_utils.sh
@@ -8,6 +8,8 @@ MICRO_NVIDIA_DRV_PREFIX=nvidia-open-driver-${NVIDIA_DRV_FAMILY}
 SLE_NVIDIA_DRV_NAME=${SLE_DRV_PREFIX}-kmp
 MICRO_NVIDIA_DRV_NAME=${MICRO_DRV_PREFIX}-signed-kmp
 
+source /etc/os-release
+
 ##############################################################################
 
 case $(whoami) in
@@ -21,13 +23,80 @@ esac
 
 ##############################################################################
 
-install_nvidia_compute_utils() {
-  source /etc/os-release
-
-  if lspci | grep -i nvidia | grep -qi vga
+check_for_nvidia_gpu() {
+  if lspci | grep -i nvidia | grep -qi vga 
   then
-    echo "NVIDIA VGA device found, checking for nvidia-compute-utils package ..."
-    if ! zypper se nvidia-compute-untils-${NVIDIA_DRV_FAMILY} | grep -q ^i
+    HAS_NVIDIA_GPU=yes
+  elif echo ${*} | grep -q force
+  then
+    HAS_NVIDIA_GPU=force
+  else
+    echo
+    echo "NVIDIA VGA device found. Exiting."
+    HAS_NVIDIA_GPU=no
+    echo
+  fi
+}
+
+add_nvidia_compute_module() {
+  if $(suseconnect -l | grep -q sle-module-NVIDIA-compute)
+  then
+  echo "Registering/Installing the NVIDIA Compute module ..."
+  echo "---------------------------------------------------------------------------"
+    echo "COMMAND: ${SUDO_CMD} suseconnect -p sle-module-desktop-applications/${VERSION_ID}/$(uname -m) --gpg-auto-import-keys"
+    ${SUDO_CMD} suseconnect -p sle-module-desktop-applications/${VERSION_ID}/$(uname -m) --gpg-auto-import-keys
+
+    echo "COMMAND: ${SUDO_CMD} suseconnect -p sle-module-development-tools/${VERSION_ID}/$(uname -m) --gpg-auto-import-keys"
+    ${SUDO_CMD} suseconnect -p sle-module-development-tools/${VERSION_ID}/$(uname -m) --gpg-auto-import-keys
+
+    echo "COMMAND: ${SUDO_CMD} suseconnect -p sle-module-NVIDIA-compute/$(echo ${VERSION_ID} | cut -d . -f 1)/$(uname -m) --gpg-auto-import-keys"
+    ${SUDO_CMD} suseconnect -p sle-module-NVIDIA-compute/$(echo ${VERSION_ID} | cut -d . -f 1)/$(uname -m) --gpg-auto-import-keys
+
+    echo
+  else
+    echo
+    echo "ERROR: Cannot find the NVIDIA Compute module ."
+    echo "       Please ensure the system has been registered with SCC or an"
+    echo "       RMT server that has the sle-module-NVIDIA-compute repo mirrored."
+    echo
+    echo "       Exiting."
+    exit
+  fi
+}
+
+install_nvidia_driver() {
+  echo "Installing ${SLE_NVIDIA_DRV_NAME} and nvidia-compute-utils-${NVIDIA_DRV_FAMILY}..."
+  echo "---------------------------------------------------------------------------"
+  echo "COMMAND: ${SUDO_CMD} zypper install -y --auto-agree-with-licenses ${SLE_NVIDIA_DRV_NAME} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}"
+  ${SUDO_CMD} zypper install -y --auto-agree-with-licenses ${SLE_NVIDIA_DRV_NAME} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}
+  echo
+}
+
+install_nvidia_compute_utils() {
+  echo "Installing nvidia-compute-utils-${NVIDIA_DRV_FAMILY} ..."
+  echo "---------------------------------------------------------------------------"
+  echo "COMMAND: ${SUDO_CMD} zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}"
+  ${SUDO_CMD} zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}
+  echo
+}
+
+install_nvidia_driver_and_compute_utils() {
+  if [ "${HAS_NVIDIA_GPU}" == yes ] || [ "${HAS_NVIDIA_GPU}" == force ] 
+  then
+    case ${HAS_NVIDIA_GPU} in
+      yes)
+        echo
+        echo "NVIDIA VGA device found, checking for nvidia-compute-utils package ..."
+        echo
+      ;;
+      force)
+        echo
+        echo "No NVIDIA VGA device found, checking for nvidia-compute-utils package anyway ..."
+        echo
+      ;;
+    esac
+
+    if ! zypper se nvidia-compute-utils-${NVIDIA_DRV_FAMILY} | grep -q ^i
     then
       case ${NAME} in
         SLES)
@@ -35,49 +104,43 @@ install_nvidia_compute_utils() {
           then 
             if ! zypper se ${SLE_NVIDIA_DRV_PREFIX} | grep -q ^i
             then
-              echo "Installing ${SLE_NVIDIA_DRV_NAME} and nvidia-compute-utils-${NVIDIA_DRV_FAMILY}..."
-              echo "COMMAND: ${SUDO_CMD} zypper install -y --auto-agree-with-licenses ${SLE_NVIDIA_DRV_NAME} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}"
-              ${SUDO_CMD} zypper install -y --auto-agree-with-licenses ${SLE_NVIDIA_DRV_NAME} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}
+              install_nvidia_driver
             else
-              echo "Installing nvidia-compute-utils-${NVIDIA_DRV_FAMILY} ..."
-              echo "COMMAND: ${SUDO_CMD} zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}"
-              ${SUDO_CMD} zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}
+              install_nvidia_compute_utils
             fi
           else
-            echo "ERROR: Cannot find the ${SLE_NVIDIA_DRV_NAME} package."
-            echo "       Please ensure the NVIDIA_Compute_Module has been added and enabled."
-            echo "       Exiting."
-            echo
-            exit
+            add_nvidia_compute_module
+            install_nvidia_driver
+            install_nvidia_compute_utils
           fi
           echo
         ;;
-        SL-Micro)
-          if zypper se ${MICRO_NVIDIA_DRV_PREFIX} | grep -q ${MICRO_NVIDIA_DRV_NAME}
-          then 
-            local NVIDIA_DRV_VER=$(zypper se -s nvidia-open-driver | grep nvidia-open-driver- | sed "s/.* package | //g" | sed "s/\s.*//g" | sort | head -n 1 | sed "s/[-_].*//g")
-
-            if ! zypper se ${MICRO_NVIDIA_DRV_PREFIX} | grep -q ^i
-            then
-              echo "Installing ${MICRO_NVIDIA_DRV_NAME} and nvidia-compute-utils-${NVIDIA_DRV_FAMILY}..."
-              echo "COMMAND: ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses ${MICRO_NVIDIA_DRV_NAME}=${NVIDIA_DRV_VER} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}"
-              ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses ${MICRO_NVIDIA_DRV_NAME}=${NVIDIA_DRV_VER} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}
-              REBOOT=Y
-            else
-              echo "Installing nvidia-compute-utils-G06 ..."
-              echo "COMMAND: ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}"
-              ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}
-              REBOOT=Y
-            fi
-          else
-            echo "ERROR: Cannot find the ${MICRO_NVIDIA_DRV_NAME} package."
-            echo "       Please ensure the repository has been added and is enabled."
-            echo "       Exiting."
-            echo
-            exit
-          fi
-          echo
-        ;;
+#        SL-Micro)
+#          if zypper se ${MICRO_NVIDIA_DRV_PREFIX} | grep -q ${MICRO_NVIDIA_DRV_NAME}
+#          then 
+#            local NVIDIA_DRV_VER=$(zypper se -s nvidia-open-driver | grep nvidia-open-driver- | sed "s/.* package | //g" | sed "s/\s.*//g" | sort | head -n 1 | sed "s/[-_].*//g")
+# 
+#            if ! zypper se ${MICRO_NVIDIA_DRV_PREFIX} | grep -q ^i
+#            then
+#              echo "Installing ${MICRO_NVIDIA_DRV_NAME} and nvidia-compute-utils-${NVIDIA_DRV_FAMILY}..."
+#              echo "COMMAND: ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses ${MICRO_NVIDIA_DRV_NAME}=${NVIDIA_DRV_VER} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}"
+#              ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses ${MICRO_NVIDIA_DRV_NAME}=${NVIDIA_DRV_VER} nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}
+#              REBOOT=Y
+#            else
+#              echo "Installing nvidia-compute-utils-G06 ..."
+#              echo "COMMAND: ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}"
+#              ${SUDO_CMD} transactional-update run zypper install -y --auto-agree-with-licenses nvidia-compute-utils-${NVIDIA_DRV_FAMILY}=${NVIDIA_DRV_VER}
+#              REBOOT=Y
+#            fi
+#          else
+#            echo "ERROR: Cannot find the ${MICRO_NVIDIA_DRV_NAME} package."
+#            echo "       Please ensure the repository has been added and is enabled."
+#            echo "       Exiting."
+#            echo
+#            exit
+#          fi
+#          echo
+#        ;;
       esac
     else
       echo "nvidia-compute-utils-${NVIDIA_DRV_FAMILY} already installed, continuing ..."
@@ -91,12 +154,20 @@ install_nvidia_compute_utils() {
           echo "COMMAND: ${SUDO_CMD} ln -s /sbin/ldconfig /sbin/ldconfig.real"
           ${SUDO_CMD} ln -s /sbin/ldconfig /sbin/ldconfig.real
           echo
+        else
+          echo
+          echo "/sbin/ldconfig.real exists, continuing ..."
+          echo
         fi
   
-        if ! grep -q ^PATH /etc/default/rke2-server
+        if ! grep -q "^PATH" /etc/default/rke2-server 2>/dev/null
         then
           echo "COMMAND: ${SUDO_CMD} echo PATH=${PATH} >> /etc/default/rke2-server"
           ${SUDO_CMD} echo PATH=${PATH} >> /etc/default/rke2-server
+          echo
+        else
+          echo
+          echo "/etc/default/rke2-server contains a line beginning with PATH, continuing ..."
           echo
         fi
       ;;
@@ -106,7 +177,12 @@ install_nvidia_compute_utils() {
 
 ##############################################################################
 
-install_nvidia_compute_utils
+check_for_nvidia_gpu ${*}
+install_nvidia_driver_and_compute_utils
+
+echo
+echo "----------  Finished  ----------"
+echo
 
 case ${REBOOT} in
   Y|y|YES|Yes|yes)
