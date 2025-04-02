dc968b16d2674b28a0f4e8cf65f68d303c063d41
roncterry <15840212+roncterry@users.noreply.github.com>
2025-04-02 10:49:51 -0600

    cert-manager from AppCo and fixed letsEncrypt

--- 02-deploy_rancher_with_helm.sh
+++ 02-deploy_rancher_with_helm.sh
@@ -17,6 +17,7 @@ then
   fi
 else
   CERTMANAGER_HELM_REPO="https://charts.jetstack.io"
+  CERTMANAGER_HELM_CHART="oci://dp.apps.rancher.io/charts/cert-manager"
   CERTMANAGER_VERSION=""
   RANCHER_HELM_REPO="https://charts.rancher.com/server-charts/prime"
   RANCHER_HOSTNAME="rancher.example.com"
@@ -30,6 +31,8 @@ else
   RANCHER_TLS_CA_FILE=
   RANCHER_TLS_PRIVATE_CA=true
 fi
+
+LICENSES_FILE=authentication_and_licenses.cfg
 
 ##############################################################################
 
@@ -91,19 +94,44 @@ check_for_helm() {
 
 ##############################################################################
 
+log_into_app_collection() {
+  if [ -z ${APP_COLLECTION_USERNAME} ]
+  then
+    # The APP_COLLECTION_URI, APP_COLLECTION_USERNAME and APP_COLLECTION_PASSWORD
+    # variables are set in an external file and are sourced in here:
+    source ${LICENSES_FILE}
+  fi
+
+  echo "Logging into the Application Collection ..."
+  echo "COMMAND: helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}"
+  helm registry login dp.apps.rancher.io/charts -u ${APP_COLLECTION_USERNAME} -p ${APP_COLLECTION_PASSWORD}
+  echo
+}
+
 install_certmanager() {
+  if ! [ -z ${CERTMANAGER_VERSION} ]
+  then
+    local CERTMANAGER_VER_ARG="--version ${CERTMANAGER_VERSION}"
+  fi
+
   echo "Installing Cert-Manager ..."
   echo "------------------------------------------------------------"
-  echo "COMMAND: 
-  helm repo add cert-manager ${CERTMANAGER_HELM_REPO}
-  helm repo update"
-
-  helm repo add cert-manager ${CERTMANAGER_HELM_REPO}
-  helm repo update
-
-  echo
-  echo "COMMAND: helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VERSION}"
-  helm install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VERSION}
+  if [ -z ${CERTMANAGER_HELM_CHART} ]
+  then
+    echo "COMMAND: 
+    helm repo add cert-manager ${CERTMANAGER_HELM_REPO}
+    helm repo update"
+ 
+    helm repo add cert-manager ${CERTMANAGER_HELM_REPO}
+    helm repo update
+ 
+    echo
+    echo "COMMAND: helm upgrade --install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VER_ARG}"
+    helm upgrade --install cert-manager cert-manager/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true ${CERTMANAGER_VER_ARG}
+  else
+    echo "COMMAND: helm upgrade --install cert-manager ${CERTMANAGER_HELM_CHART} --namespace cert-manager --create-namespace --set 'global.imagePullSecrets[0].name'=${IMAGE_PULL_SECRET_NAME} --set crds.enabled=true ${CERTMANAGER_VER_ARG}"
+    helm upgrade --install cert-manager ${CERTMANAGER_HELM_CHART} --namespace cert-manager --create-namespace --set 'global.imagePullSecrets[0].name'=${IMAGE_PULL_SECRET_NAME} --set crds.enabled=true ${CERTMANAGER_VER_ARG}
+  fi
 
   echo
   echo "COMMAND: kubectl -n cert-manager rollout status deploy/cert-manager"
@@ -122,6 +150,13 @@ install_rancher() {
       curl https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem --output cacerts.pem
       echo
 
+      if ! kubectl get namespaces | grep -q cattle-system
+      then
+        echo "Creating cattle-system namespace ..."
+        echo "COMMAND: kubectl create namespace cattle-system"
+        kubectl create namespace cattle-system
+      fi
+
       echo "Creating secret for the CA cert ..."
       echo "COMMAND: kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem"
       kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem
@@ -130,6 +165,13 @@ install_rancher() {
       echo
     ;;
     secret)
+      if ! kubectl get namespaces | grep -q cattle-system
+      then
+        echo "Creating cattle-system namespace ..."
+        echo "COMMAND: kubectl create namespace cattle-system"
+        kubectl create namespace cattle-system
+      fi
+
       echo "Creating TLS secret ..."
       echo "COMMAND: kubectl -n cattle-system create secret tls tls-rancher-ingress --cert=${RANCHER_TLS_CERT_FILE} --key=${RANCHER_TLS_KEY_FILE}"
       kubectl -n cattle-system create secret tls tls-rancher-ingress --cert=${RANCHER_TLS_CERT_FILE} --key=${RANCHER_TLS_KEY_FILE}
